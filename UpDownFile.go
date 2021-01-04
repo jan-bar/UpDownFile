875e0f99dd78babfe13c4bea8581e411ce22a0d5
janbar <janbar@163.com>
2021-01-04 21:07:46 +0800

    完善上传和排序

--- UpDownFile.go
+++ UpDownFile.go
@@ -1,308 +1,322 @@
 package main
 
 import (
-    "bufio"
-    "encoding/base64"
-    "errors"
     "fmt"
     "io"
     "io/ioutil"
-    "net"
+    "net/http"
     "net/url"
     "os"
     "path/filepath"
+    "sort"
     "strconv"
     "strings"
 )
 
-var authStr string // 授权信息
+var basePath string
 
 func main() {
-    var path, user, pass string
     switch len(os.Args) {
-    case 2: // 当前目录
-        path = "."
-    case 3: // 无认证模式
-        path = os.Args[2]
-    case 5: // 添加用户名密码认证
-        path, user, pass = os.Args[2], os.Args[3], os.Args[4]
-        authStr = "Basic " + base64.StdEncoding.EncodeToString([]byte(user+":"+pass))
+    case 2:
+        basePath = "."
+    case 3:
+        basePath = os.Args[2]
     default:
-        fmt.Printf("usage: %s ip:port [path] [user] [pass]\n", os.Args[0])
+        fmt.Printf("usage: %s ip:port [path]\n", os.Args[0])
+        return
+    }
+    var err error
+    basePath, err = filepath.Abs(basePath)
+    if err != nil {
+        fmt.Println(err)
         return
     }
 
-    addr, err := net.ResolveTCPAddr("tcp", os.Args[1])
+    http.HandleFunc("/", upDownFile)
+    fmt.Printf("handle [%s] [%s]\n", os.Args[1], basePath)
+    err = http.ListenAndServe(os.Args[1], nil)
     if err != nil {
-        panic(err)
-    }
-    path, err = filepath.Abs(path)
-    if err != nil {
+        fmt.Println(err)
         return
     }
+}
 
-    ser, err := net.ListenTCP("tcp", addr)
-    if err != nil {
-        panic(err)
-    }
-
-    if authStr != "" {
-        fmt.Printf(`get file:
-  wget --auth-no-challenge --user=%s --password=%s --content-disposition "http://%s?path"
-  curl -u %s:%s -OJ "http://%s?path"
-post file:
-  wget -qO - --auth-no-challenge --user=%s --password=%s --post-file=C:\file "http://%s?path"
-  curl -u %s:%s --data-binary @C:\file "http://%s?path"
-`, user, pass, addr, user, pass, addr, user, pass, addr, user, pass, addr)
+func upDownFile(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodGet {
+        err := handleGetFile(w, r)
+        if err != nil {
+            w.Header().Set("Content-Type", "text/html;charset=utf-8")
+            w.Write([]byte("<html><head><title>message</title></head><body><center><h2>"))
+            w.Write([]byte(err.Error()))
+            w.Write([]byte("</h2></center></body></html>"))
+        }
+    } else if r.Method == http.MethodPost {
+        err := handlePostFile(w, r)
+        if err == nil {
+            w.Write([]byte("ok"))
+        } else {
+            w.Write([]byte(err.Error()))
+        }
     } else {
-        fmt.Printf(`get file:
-  wget --content-disposition "http://%s?path"
-  curl -OJ "http://%s?path"
-post file:
-  wget -qO - --post-file=C:\file "http://%s?path"
-  curl --data-binary @C:\file "http://%s?path"
-`, addr, addr, addr, addr)
-    }
-    for {
-        ln, err := ser.AcceptTCP()
-        if err != nil {
-            panic(err)
-        }
-        go func(l *net.TCPConn) {
-            h := &httpHandle{
-                path:   path,
-                header: make(map[string]string, 4),
-                r:      bufio.NewReader(l),
-                w:      bufio.NewWriter(l),
-            }
-            err := handleFile(h)
-            if err != nil {
-                h.respMessage(err.Error())
-            }
-            h.w.Flush()
-            l.Close()
-        }(ln)
+        w.Write([]byte(r.Method + " not support"))
     }
 }
 
-const (
-    httpGet   = "GET"
-    httpPost  = "POST"
-    resHeader = "HTTP/1.1 200 OK\r\nContent-Type:text/html;charset=utf-8\r\n\r\n"
-    getHeader = "HTTP/1.1 200 OK\r\nContent-Type:application/octet-stream\r\nContent-Disposition:attachment;filename=%s\r\nContent-Length:%d\r\nContent-Transfer-Encoding:binary\r\n\r\n"
-)
-
-func handleFile(h *httpHandle) error {
-    err := h.getHeader()
+func handlePostFile(_ http.ResponseWriter, r *http.Request) error {
+    fr, fh, err := r.FormFile("upload")
     if err != nil {
         return err
     }
-    if h.method == httpGet {
-        return h.get()
-    }
-    return h.post()
-}
+    defer fr.Close()
 
-type httpHandle struct {
-    path    string
-    method  string
-    urlPath string
-    header  map[string]string
-    r       *bufio.Reader
-    w       *bufio.Writer
-}
-
-func (h *httpHandle) respMessage(data string) {
-    h.w.WriteString("HTTP/1.1 200 OK\r\nContent-Type:text/html;charset=utf-8\r\n\r\n<html><head><title>message</title></head><body><center><h2>")
-    h.w.WriteString(data)
-    h.w.WriteString("</h2></center></body></html>")
-}
-
-func (h *httpHandle) getHeader() error {
-    // 读取第一行,提取有用信息
-    line, _, err := h.r.ReadLine()
+    path := filepath.Join(basePath, r.URL.Path, fh.Filename)
+    fw, err := os.Create(path)
     if err != nil {
         return err
     }
-    header := strings.Fields(string(line))
-    if len(header) < 3 { // 首行至少3列数据
-        return errors.New("header error")
-    }
-    h.method = header[0]
-    if h.method != httpGet && h.method != httpPost {
-        return errors.New(h.method + " not support")
-    }
-    u, err := url.Parse(header[1])
-    if err != nil {
-        return err
-    }
-    h.urlPath = u.Path
-
-    for {
-        line, _, err = h.r.ReadLine()
-        if err != nil {
-            return err
-        }
-        if len(line) == 0 {
-            break // 遇到空行,之后为请求体
-        }
-        tmp := string(line)
-        if index := strings.Index(tmp, ":"); index > 0 { // key: val
-            h.header[strings.ToLower(strings.TrimSpace(tmp[:index]))] = strings.TrimSpace(tmp[index+1:])
-        }
-    }
-    return nil
+    pr := newProgress(fr, "POST "+path, fh.Size)
+    _, err = io.Copy(fw, pr)
+    fw.Close() // 刷新到文件
+    pr.Close()
+    return err
 }
 
-func (h *httpHandle) get() error {
-    path := filepath.Join(h.path, h.urlPath)
+func handleGetFile(w http.ResponseWriter, r *http.Request) error {
+    path := filepath.Join(basePath, r.URL.Path)
     fi, err := os.Stat(path)
     if err != nil {
         return err
     }
 
     if fi.IsDir() {
-        dir, err := ioutil.ReadDir(path)
+        sortType, _ := strconv.Atoi(r.FormValue("sort"))
+        dir, err := sortDir(path, sortDirType(sortType))
         if err != nil {
             return err
         }
-        h.w.WriteString("HTTP/1.1 200 OK\r\nContent-Type:text/html;charset=utf-8\r\n\r\n<html><head><title>list dir</title></head><body>")
-        h.w.WriteString(`<div style="position:fixed;bottom:20px;right:20px">
-<form action="` + h.urlPath + `" method="POST" enctype="multipart/form-data">
-    <p><input type="file" name="file"></p>
-    <p><input type="submit" value="上传文件"></p>
-</form>
-<input type="button" onclick="javascript:window.history.back()" value="后退"/>
-<input type="button" onclick="javascript:window.history.forward()" value="前进" style="margin:5px"/>
-<a href="#top" style="margin:5px">顶部</a>
-<a href="#bottom">底部</a>
-</div>`)
-        h.w.WriteString("<table border=\"1\" align=\"center\"><tr><th>类型</th><th>大小</th><th>修改时间</th><th>链接</th></tr>")
-        for _, v := range dir {
-            h.w.WriteString("<tr><td>")
+        w.Write(htmlPrefix)
+        for i, v := range dir {
+            w.Write(htmlTrTd)
+            w.Write([]byte(strconv.Itoa(i + 1)))
+            w.Write(htmlTdTd)
             if v.IsDir() {
-                h.w.WriteByte('D')
+                w.Write(htmlDir)
             } else {
-                h.w.WriteByte('F')
+                w.Write(htmlFile)
             }
-            h.w.WriteString("</td><td>")
-            h.w.WriteString(convertByte(v.Size()))
-            h.w.WriteString("</td><td>")
-            h.w.WriteString(v.ModTime().Format("2006-01-02 15:04:05"))
-            h.w.WriteString("</td><td><a href=\"")
-            // 对文件或目录进行拼接
-            h.w.WriteString(url.QueryEscape(strings.TrimLeft(h.urlPath+"/"+v.Name(), "/")))
-            h.w.WriteString("\">")
-            h.w.WriteString(v.Name())
-            h.w.WriteString("</a></td></tr>")
+            w.Write(htmlTdTd)
+            w.Write([]byte(convertByte(v.Size())))
+            w.Write(htmlTdTd)
+            w.Write([]byte(v.ModTime().Format(timeLayout)))
+            w.Write(htmlTdTdA)
+            w.Write([]byte(url.PathEscape(strings.TrimLeft(r.URL.Path+"/"+v.Name(), "/"))))
+            w.Write(htmlGt)
+            w.Write([]byte(v.Name()))
+            w.Write(htmlAtdTr)
         }
-        h.w.WriteString("</table><a name=\"bottom\"></a></body></html>")
+        w.Write(htmlSuffix)
         return nil
     }
 
-    size := fi.Size()
     fr, err := os.Open(path)
     if err != nil {
         return err
     }
-    fmt.Fprintf(h.w, getHeader, filepath.Base(path), size)
-    pr := newProgress(fr, path, size)
-    _, err = io.Copy(h.w, pr)
+    defer fr.Close()
+
+    size := fi.Size()
+    wh := w.Header()
+    wh.Set("Content-Type", "application/octet-stream")
+    wh.Set("Content-Length", strconv.FormatInt(size, 10))
+    wh.Set("Content-Disposition", "attachment;filename="+filepath.Base(r.URL.Path))
+    wh.Set("Content-Transfer-Encoding", "binary")
+    pr := newProgress(fr, "GET "+path, size)
+    _, err = io.Copy(w, pr)
     pr.Close()
-    fr.Close()
     return err
 }
 
-func (h *httpHandle) post() error {
-    var size int64
-    if tmp, ok := h.header["content-length"]; ok {
-        size, _ = strconv.ParseInt(tmp, 10, 0)
+const timeLayout = "2006-01-02 15:04:05"
+
+var (
+    htmlTrTd   = []byte("<tr><td>")
+    htmlDir    = []byte{'D'}
+    htmlFile   = []byte{'F'}
+    htmlTdTd   = []byte("</td><td>")
+    htmlTdTdA  = []byte("</td><td><a href=\"")
+    htmlGt     = []byte("\">")
+    htmlAtdTr  = []byte("</a></td></tr>")
+    htmlPrefix = []byte(`<html><head><title>list dir</title></head><body><div style="position:fixed;bottom:20px;right:20px">
+<p><label><input type="radio" name="sort" onclick="sortDir(0)">文件名升序</label></p>
+<p><label><input type="radio" name="sort" onclick="sortDir(1)">文件名降序</label></p>
+<p><label><input type="radio" name="sort" onclick="sortDir(2)">时间升序</label></p>
+<p><label><input type="radio" name="sort" onclick="sortDir(3)">时间降序</label></p>
+<p><label><input type="radio" name="sort" onclick="sortDir(4)">大小升序</label></p>
+<p><label><input type="radio" name="sort" onclick="sortDir(5)">大小降序</label></p>
+
+<p><input type="file" id="upload"></p>
+<progress value="0" id="progress"></progress>
+<p><input type="button" onclick="uploadFile()" value="上传文件"></p>
+
+<input type="button" onclick="backSuper()" value="返回上级"/>
+<a href="#top">顶部</a>
+<a href="#bottom">底部</a>
+</div>
+<script>
+function uploadFile() {
+    var upload=document.getElementById('upload').files[0];
+    if (!upload) {
+        alert('请选择上传文件');
+        return
     }
-    if size <= 0 {
-        return errors.New("content-length error")
+    var params = new FormData();
+    params.append('upload', upload);
+    var xhr = new XMLHttpRequest();
+    xhr.onerror = function () {
+        alert('请求失败');
     }
-    var boundary string
-    if tmp, ok := h.header["content-type"]; ok {
-        for _, v := range strings.Split(tmp, ";") {
-            if v = strings.TrimSpace(v); v == "multipart/form-data" {
-                ok = false
-            } else if strings.HasPrefix(v, "boundary=") {
-                boundary = "--" + v[9:]
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                if (xhr.responseText == "ok") {
+                    window.location.reload();
+                }else{
+                    alert(xhr.responseText);
+                }
+            } else {
+                console.error(xhr.status)
             }
         }
-        if ok { // 没有multipart/form-data则置空
-            boundary = ""
-        }
     }
-    if boundary == "" {
-        return errors.New("content-type error")
+    var progress=document.getElementById('progress');
+    xhr.upload.onprogress = function (e) {
+        progress.value = e.loaded;
+        progress.max = e.total;
     }
+var url=new URL(window.location.href);
+    xhr.open('POST', url.pathname, true);
+    xhr.send(params);
+}
+function sortDir(type) {
+    var url=window.location.href;
+    var i=url.indexOf('?');
+if (i>0){
+window.location.href=url.substr(0,i)+'?sort='+type;
+}else{
+window.location.href=url+'?sort='+type;
+}
+}
+function backSuper(type) {
+    var url=window.location.href;
+    var i=url.indexOf('?');
+if (i>0){
+i=url.substring(0,i).lastIndexOf('/');
+if (i>0){
+window.location.href=url.substr(0,i);
+}
+}else{
+i=url.lastIndexOf('/');
+if (i>0){
+window.location.href=url.substr(0,i);
+}
+}
+}
+</script>
+<table border="1" align="center"><tr><th>序号</th><th>类型</th><th>大小</th><th>修改时间</th><th>链接</th></tr>`)
+    htmlSuffix = []byte("</table><a name=\"bottom\"></a></body></html>")
+)
 
-    line, _, err := h.r.ReadLine()
-    if err != nil {
-        return err
-    }
-    if string(line) != boundary {
-        return errors.New(boundary + " != " + string(line))
-    }
-    delSize := len(boundary) + 4
+/*--------------------------------下面是工具类---------------------------------*/
+const (
+    sortDirTypeByNameAsc  sortDirType = iota // 文件名升序
+    sortDirTypeByNameDesc                    // 文件名降序
+    sortDirTypeByTimeAsc                     // 时间升序
+    sortDirTypeByTimeDesc                    // 时间降序
+    sortDirTypeBySizeAsc                     // 文件大小升序
+    sortDirTypeBySizeDesc                    // 文件大小降序
+)
 
-    for {
-        line, _, err = h.r.ReadLine()
-        if err != nil {
-            return err
-        }
-        if len(line) == 0 {
-            break // 遇到空行,之后为请求体
-        }
-        delSize += len(line) + 2
-        tmp := string(line)
-        if index := strings.Index(tmp, ":"); index > 0 { // key: val
-            h.header[strings.ToLower(strings.TrimSpace(tmp[:index]))] = strings.TrimSpace(tmp[index+1:])
-        }
+type (
+    sortDirType int
+    dirInfoSort struct {
+        fi       []os.FileInfo
+        sortType sortDirType
     }
-    var filename string
-    if tmp, ok := h.header["content-disposition"]; ok {
-        for _, v := range strings.Split(tmp, ";") {
-            if v = strings.TrimSpace(v); v == "form-data" {
-                ok = false
-            } else if strings.HasPrefix(v, "filename=") {
-                filename = strings.Trim(v[9:], "\"")
-            }
-        }
-        if ok { // 没有multipart/form-data则置空
-            filename = ""
-        }
-    }
-    size -= int64(delSize)
+)
 
-    fw, err := os.Create(filename)
+func sortDir(dir string, sortType sortDirType) ([]os.FileInfo, error) {
+    fi, err := ioutil.ReadDir(dir)
     if err != nil {
-        return err
+        return nil, err
     }
-    defer fw.Close()
-    pr := newProgress(h.r, filename, size)
-    _, err = io.CopyN(fw, pr, size)
-    pr.Close()
-    if err != nil {
-        return err
-    }
-    err = fw.Truncate(size - int64(len(boundary)) - 6)
-    if err != nil {
-        return err
-    }
-    h.respMessage("post ok")
-    return nil
+    sort.Sort(&dirInfoSort{fi: fi, sortType: sortType})
+    return fi, nil
 }
 
-/* 下面是工具类 */
+func (d *dirInfoSort) Len() int {
+    return len(d.fi)
+}
+func (d *dirInfoSort) Default(x, y int) bool {
+    // 当其他选项比较相等时,使用文件名升序排序
+    lx, ly := len(d.fi[x].Name()), len(d.fi[y].Name())
+    if lx == ly {
+        return d.fi[x].Name() < d.fi[y].Name()
+    }
+    return lx < ly
+}
+func (d *dirInfoSort) Less(x, y int) bool {
+    if d.fi[x].IsDir() != d.fi[y].IsDir() {
+        return d.fi[x].IsDir() // 目录永远在前面
+    }
+    switch d.sortType {
+    default:
+        fallthrough // 不在范围内采取文件名升序
+    case sortDirTypeByNameAsc:
+        return d.Default(x, y)
+    case sortDirTypeByNameDesc:
+        lx, ly := len(d.fi[x].Name()), len(d.fi[y].Name())
+        if lx == ly {
+            return d.fi[x].Name() > d.fi[y].Name()
+        }
+        return lx > ly
+    case sortDirTypeByTimeAsc:
+        tx, ty := d.fi[x].ModTime(), d.fi[y].ModTime()
+        if tx.Unix() == ty.Unix() {
+            return d.Default(x, y)
+        }
+        return tx.Before(ty)
+    case sortDirTypeByTimeDesc:
+        tx, ty := d.fi[x].ModTime(), d.fi[y].ModTime()
+        if tx.Unix() == ty.Unix() {
+            return d.Default(x, y)
+        }
+        return tx.After(ty)
+    case sortDirTypeBySizeAsc:
+        sx, sy := d.fi[x].Size(), d.fi[y].Size()
+        if sx == sy {
+            return d.Default(x, y)
+        }
+        return sx < sy
+    case sortDirTypeBySizeDesc:
+        sx, sy := d.fi[x].Size(), d.fi[y].Size()
+        if sx == sy {
+            return d.Default(x, y)
+        }
+        return sx > sy
+    }
+}
+func (d *dirInfoSort) Swap(x, y int) {
+    d.fi[x], d.fi[y] = d.fi[y], d.fi[x]
+}
+
+/* 读IO加进度 */
 type progress struct {
     r    io.Reader
     cnt  int64
     rate chan int64
 }
 
-func newProgress(r io.Reader, file string, size int64) io.ReadCloser {
+func newProgress(r io.Reader, prefix string, size int64) io.ReadCloser {
     cnt := 0
     for tmp := size; tmp > 0; tmp /= 10 {
         cnt++
@@ -314,8 +328,8 @@ func newProgress(r io.Reader, file string, size int64) io.ReadCloser {
         for cur := range rate {
             fmt.Printf(format, cur)
         }
-        fmt.Printf(format, size)
-    }(p.rate, fmt.Sprintf("\r%s [%%%dd - %d]", file, cnt, size), size)
+        fmt.Printf(format+"\r\n\r\n", size)
+    }(p.rate, fmt.Sprintf("\r%s [%%%dd - %d]", prefix, cnt, size), size)
     return p
 }
 
